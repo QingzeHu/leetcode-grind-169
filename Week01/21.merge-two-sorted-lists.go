5ecbed204b6c9ec6f1f48c6642b77fa10467111e
Qingze Hu <qingzeinus@gmail.com>
2025-01-07 23:58:06 -0600

    Implement mergeTwoLists function to merge two sorted linked lists

--- Week01/21.merge-two-sorted-lists.go
+++ Week01/21.merge-two-sorted-lists.go
@@ -13,7 +13,26 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    
+	preHead := &ListNode{-1, nil}
+	prev := preHead
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			prev.Next = list1
+			list1 = list1.Next
+		} else {
+			prev.Next = list2
+			list2 = list2.Next
+		}
+		prev = prev.Next
+	}
+	if list1 != nil {
+		prev.Next = list1
+	}
+	if list2 != nil {
+		prev.Next = list2
+	}
+	return preHead.Next
 }
+
 // @lc code=end
 
